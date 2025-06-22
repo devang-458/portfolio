9d2ac9c33a71822a6b39c606bae9942ce1d2e90a
Devang <deekayedm@gmail.com>
2025-06-22 15:23:52 +0530

    astronaut added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import Hero from "./sections/Hero";
+import Navbar from "./sections/Navbar";
+
 function App() {
-  return <div>hi</div>;
+  return (
+    <div className="container mx-auto max-w-7xl ">
+      <Navbar />
+      <Hero />
+      <section className="min-h-screen" />
+      <section className="min-h-screen" />
+      <section className="min-h-screen" />
+      <section className="min-h-screen" />
+      <section className="min-h-screen" />
+    </div>
+  );
 }
 
 export default App;
